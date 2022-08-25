b7ede47ae708b2f71f61426d45b82b7e6795908a
DESKTOP-C2RSHRK\zhuji <yifengzhao0530@gmail.com>
2022-08-25 22:55:08 +1000

    Init project; Add products table.

---
+++ KolmeoTest/Data/KolmeoTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KolmeoTest;
+
+namespace KolmeoTest.Data
+{
+    public class KolmeoTestContext : DbContext
+    {
+        public KolmeoTestContext (DbContextOptions<KolmeoTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = default!;
+    }
+}
